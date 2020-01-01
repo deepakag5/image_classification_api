f3c39f81c963bd7e35a08455ae55510b56126da2
iamDeepakA <deepakagarwal@gwu.edu>
2020-01-01 12:10:07 -0500

    added class DatasetProcessing in train model

--- model/train_model.py
+++ model/train_model.py
@@ -15,3 +15,22 @@ import torchvision.transforms as transforms
 from PIL import Image  # For handling the images
 from sklearn.model_selection import train_test_split
 from torch.utils.data import Dataset
+
+
+#specify the class for preprocessing the data
+class DatasetProcessing(Dataset):
+    """
+    This function is used to initialise the class variables - transform, data, target
+
+    """
+
+    def __init__(self, data, target, transform=None):
+        self.transform = transform
+        self.data = data.reshape((-1, 120, 320)).astype(np.uint8)[:, :, :, None]
+        self.target = torch.from_numpy(target).float()
+
+    def __getitem__(self, index):
+        return self.transform(self.data[index]), self.target[index]
+
+    def __len__(self):
+        return len(list(self.data))
