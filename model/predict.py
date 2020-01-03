35693ba0bf6ed5267af30abc7ddb5be1fa18e1c8
iamDeepakA <deepakagarwal@gwu.edu>
2020-01-02 22:02:11 -0500

    added CNN class in predict file

--- model/predict.py
+++ model/predict.py
@@ -1,11 +1,10 @@
-Loading
-required
-packages
+# Loading required packages
 import numpy as np
 
 # importing torch packages
 import torch
 from torch.utils.data import Dataset
+import torch.nn as nn
 
 
 # This class to transform the data, so that it can be loaded to test Loader
@@ -26,3 +25,44 @@ class DatasetProcessing(Dataset):
 
     def __len__(self):  # returns the length of the data
         return len(list(self.data))
+
+
+# specify the model class
+# CNN module, this is network architecture of model
+class CNN(nn.Module):
+    """
+    Here we are using CNN model with three conv layers with maxpool
+    and relu as transfer/activation function
+    for fully connected layer again we have used relu activation function
+    """
+
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(1, 32, kernel_size=5, stride=2),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2))
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(32, 64, kernel_size=3, stride=2),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2))
+        self.layer3 = nn.Sequential(
+            nn.Conv2d(64, 64, kernel_size=3, stride=2),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2))
+        self.classifier = nn.Sequential(
+            nn.Linear(256, 128),
+            nn.ReLU(),
+            nn.Linear(128, 10)
+        )
+
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = out.view(out.size(0), -1)
+        out = self.classifier(out)
+        return out
