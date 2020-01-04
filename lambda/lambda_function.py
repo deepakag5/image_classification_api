b59b54e7fb9f6ab1274fb658d5927f08bb2ea308
iamDeepakA <deepakagarwal@gwu.edu>
2020-01-04 11:22:07 -0500

    completed lambda and sqs message file

---
+++ lambda/lambda_function.py
@@ -0,0 +1,40 @@
+import json
+import boto3
+
+
+def put_message_in_queue(sqs, key_name, bucketName, QueueName='image_classification_queue'):
+    Queue_url = sqs.get_queue_url(QueueName=QueueName)['QueueUrl']
+    response = sqs.send_message(
+        QueueUrl=Queue_url,
+        DelaySeconds=10,
+        MessageAttributes={
+            'Title': {
+                'DataType': 'String',
+                'StringValue': 'image_classification_pipeline'
+            },
+            's3_key': {
+                'DataType': 'String',
+                'StringValue': key_name,
+
+            },
+            'bucketName': {
+                'DataType': 'String',
+                'StringValue': bucketName
+
+            },
+        },
+        MessageBody=(
+            'Queue_Data'
+        )
+    )
+
+
+def lambda_handler(event, context):
+    sqs = boto3.client('sqs')
+    bucketName = event['Records'][0]['s3']['bucket']['name']
+    key_name = event['Records'][0]['s3']['object']['key']
+    put_message_in_queue(sqs, key_name, bucketName)
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
